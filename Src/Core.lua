c85eb0510a1299c42c12bf642a439c8ee5a4a693
TenaarFeiri <tenaarfeiri@gmail.com>
2025-12-26 13:12:29 +0100

    Update Core.lua
    Added some minor configuration options, but no way to interact with, or remember them yet.

--- Src/Core.lua
+++ Src/Core.lua
@@ -3,7 +3,14 @@ local YapperName, YapperTable = ...
 
 YapperTable.Debug = false -- True when debugging, false otherwise.
 YapperTable.Core = {} -- Here is where the core functions live.
-
+YapperTable.Configs = {
+    Yapper = {
+        VERBOSE = false
+    }
+    Chat = {
+        USE_DELINEATORS = true   
+    }
+}
 YapperTable.Defaults = { -- Create some default values for things.
     ID = {
         Frames = {
@@ -11,7 +18,7 @@ YapperTable.Defaults = { -- Create some default values for things.
         }
     },
     Chat = {
-        CharacterLimit = 255, -- Blizzard's standard limit.
+        CharacterLimit = 255,
         MaxHistoryLines = 15
     }
 }
@@ -22,4 +29,22 @@ YapperTable.Defaults = { -- Create some default values for things.
 -- get the addon version as a string.
 function YapperTable.Core:GetYapperVersion()
     return C_AddOns.GetAddOnMetadata(YapperName, "Version")
+end
+
+-- Different settings for things.
+--- Set whether Yapper will be verbose or not!
+function YapperTable.Core:SetVerbose(Bool)
+    if type(Bool) ~= "boolean" then
+        if YapperTable.Error then
+            YapperTable.Error:PrintError("BAD_TYPE", "SetVerbose expected a boolean, got " .. type(Bool))
+        else
+            print("Yapper: SetVerbose expected a boolean, got " .. type(Bool))
+        end
+        return
+    end
+    YapperTable.Configs.Yapper.VERBOSE = Bool
+end
+
+function YapperTable.Core:GetVerbose()
+    return YapperTable.Configs.Yapper.VERBOSE
 end
